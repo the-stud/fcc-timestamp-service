190296fd4939ceefa4054170ea0c6b30b964fa39
System <none>
2017-07-05 19:08:09 +0000

    Checkpoint

--- server.js
+++ server.js
@@ -13,25 +13,15 @@ app.use(express.static('public'));
 
 // http://expressjs.com/en/starter/basic-routing.html
 app.get("/", function (request, response) {
+  console.log('root');
   response.sendFile(__dirname + '/views/index.html');
 });
 
-app.get("/dreams", function (request, response) {
-  response.send(dreams);
+app.get("/:timestamp", function (request, response) {
+  const timestamp = request.params.timestamp;
+  console.log('timestamp: ' + timestamp);
+  
 });
-
-// could also use the POST body instead of query string: http://expressjs.com/en/api.html#req.body
-app.post("/dreams", function (request, response) {
-  dreams.push(request.query.dream);
-  response.sendStatus(200);
-});
-
-// Simple in-memory store for now
-var dreams = [
-  "Find and count some sheep",
-  "Climb a really tall mountain",
-  "Wash the dishes"
-];
 
 // listen for requests :)
 var listener = app.listen(process.env.PORT, function () {
